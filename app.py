2bab492ad02ff9161d0cb2f114dcf3c06122281f
DanaArnonPerry <dana@2d-point.com>
2025-08-24 16:38:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ div[data-testid="stProgressBar"]{
   position: sticky;
   top: 10px;          /* מתחת ל-#fixed-timer (שגובהו ~36–40px) */
   z-index: 20;       /* נמוך מהטיימר (9999) */
-  margin-top: -150px;    /* ריווח קטן מהרכיב שמעל */
+  margin-top: -200px;    /* ריווח קטן מהרכיב שמעל */
   margin-bottom: 8px; /* הוסף רווח תחתון קטן */
 }
 </style>
