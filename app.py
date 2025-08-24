bdb374d555800e241e39c61aa0ce8725303ddf94
DanaArnonPerry <dana@2d-point.com>
2025-08-24 15:06:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ st.markdown("""
 :root{
   --graph-top: 28px;
   --buttons-up: -18px;
-  --question-top: 8px;      /* כמה להוריד/להעלות את השאלה */
+  --question-top: 40px;      /* כמה להוריד/להעלות את השאלה */
   --question-bottom: 12px;  /* רווח מתחת לשאלה */
 }
 .question-text{
