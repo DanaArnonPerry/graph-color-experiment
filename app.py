0af9fcb09752cf577798851ca7ec4f7a7777b011
DanaArnonPerry <dana@2d-point.com>
2025-08-24 16:05:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ st.markdown("""
   --graph-top: -20px;
   --buttons-up: -80px;
   --question-top: -80px;      /* כמה להוריד/להעלות את השאלה */
-  --question-bottom: 4px;  /* רווח מתחת לשאלה */
+  --question-bottom: 6px;  /* רווח מתחת לשאלה */
 }
 .question-text{
   text-align: center !important;               /* מרכז אופקית */
