22266753e3b627f3bccbd771618858d62b9f3dfa
DanaArnonPerry <dana@2d-point.com>
2025-08-24 16:03:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -271,7 +271,7 @@ st.markdown("""
 <style>
 /* ===== גודל, מרווח ופונט של כפתורי ה-radio ===== */
 :root{
-  --choice-size: 100px !important;     /* קוטר הכפתור (גובה+רוחב) */
+  --choice-size: 130px !important;     /* קוטר הכפתור (גובה+רוחב) */
   --choice-font: 32px !important;     /* גודל האות בתוך הכפתור */
   --choice-gap: 18px !important;      /* מרווח בין הכפתורים */
   --choice-paddingY: 4px;  /* רווח אנכי מסביב לשורה */
